67ee4b2a89489a3a199859aeea203a6b3cb886dc
jarilaitinen <jarijuhanilaitinen@gmail.com>
2025-01-08 23:23:01 +0200

    start localisation features

---
+++ src/_data/site.js
@@ -0,0 +1,19 @@
+module.exports = {
+    buildTime: new Date(),
+    baseUrl: "https://www.orbemporium.fi",
+    name: "Jari Virkki",
+    fi: {
+      metaTitle: "Jari Virkki",
+      metaDescription: "Netti- ja softwareratkaisut, audio- ja mediapalvelut",
+      contact: "Ota yhteyttä",
+      email: "Sähköposti",
+      phone: "Puhelin"
+    },
+    en: {
+        metaTitle: "Jari Virkki",
+        metaDescription: "Custom web and software solutions, audio and media services",
+        contact: "Contact me",
+        email: "Email",
+        phone: "Phone"
+      },
+  };
